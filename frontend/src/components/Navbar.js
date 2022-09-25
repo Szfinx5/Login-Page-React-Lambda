35f320ccb6bc828f33286afcb4a2889711e054dd
ghavasi <g.havasi@outlook.com>
2022-09-25 20:18:05 +0100

    Initial commit for the front end

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Cromwell developer test</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
